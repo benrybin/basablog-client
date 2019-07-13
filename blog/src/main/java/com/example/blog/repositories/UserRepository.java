df94dcc201c3502c2c0b5c073b6313ecf3d0b5bb
andrewdimaio <andrewdimaio777@gmail.com>
2019-07-13 17:15:42 -0400

    fixed

--- blog/src/main/java/com/example/blog/repositories/UserRepository.java
+++ blog/src/main/java/com/example/blog/repositories/UserRepository.java
@@ -4,11 +4,8 @@ import com.example.blog.entities.Users;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface UserRepository extends CrudRepository<Users, Integer>{
-<<<<<<< HEAD:blog/src/main/java/com/example/blog/repositories/UserRepository.java
-=======
-    List<Users> username(String username);
-
->>>>>>> parent of deb3534... fixed new table issue:blog/src/main/java/com/example/blog/UserRepository.java
 }
