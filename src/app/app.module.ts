25145399eaee0e2301ce25883948d022eb8db050
Alicia Carrion <alicia.r.carrion@gmail.com>
2019-07-13 21:11:58 -0400

    changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,13 +8,8 @@ import { TagsComponent } from './tags/tags.component';
 import { PostsComponent } from './posts/posts.component';
 import { PostcommentsComponent } from './postcomments/postcomments.component';
 import { PostDetailsComponent } from './post-details/post-details.component';
-<<<<<<< HEAD
-import { HttpClientModule } from '@angular/common/http';
-
-=======
 import { CommentsListComponent } from './comments-list/comments-list.component';
 import {HttpClientModule} from "@angular/common/http";
->>>>>>> f6f91bc3e717f8ec94fadcad4f9f1d231be8d44f
 
 @NgModule({
   declarations: [
