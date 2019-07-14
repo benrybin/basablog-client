0781d7148b7443a7c8244acb2376689841e543e0
Alicia Carrion <alicia.r.carrion@gmail.com>
2019-07-13 23:42:42 -0400

    clean project

--- src/main/resources/static/src/app/comment.ts
+++
@@ -1,9 +0,0 @@
-export class Comment {
-    id: number;
-    body: string;
-    postText : string;
-    postid: number;
-    userId : number;
-    constructor(){};
-}
-
--- src/main/resources/static/src/app/comments.spec.ts
+++
@@ -1,7 +0,0 @@
-import { Comment } from './comment';
-
-describe('Comments', () => {
-  it('should create an instance', () => {
-    expect(new Comment()).toBeTruthy();
-  });
-});
