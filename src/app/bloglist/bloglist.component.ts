53664273b73d5ace6ba4addcb51d94f8dc956a26
Alicia Carrion <alicia.r.carrion@gmail.com>
2019-07-31 21:49:53 -0400

    search

--- src/app/bloglist/bloglist.component.ts
+++ src/app/bloglist/bloglist.component.ts
@@ -4,17 +4,18 @@ import {Post} from 'src/app/post';
 import {Comment} from 'src/app/comment';
 import { FilterPipe } from '../filter.pipe';
 import { ActivatedRoute } from '@angular/router';
-import {FormsModule} from '@angular/forms';
+
 
 @Component({
   selector: 'app-bloglist',
   templateUrl: './bloglist.component.html',
   styleUrls: ['./bloglist.component.css']
 })
+
 export class BloglistComponent implements OnInit {
   filter:FilterPipe;
   posts: Post[];
-
+  search : string;
   constructor(private blogService: BlogService,
     private route: ActivatedRoute
     ) {
